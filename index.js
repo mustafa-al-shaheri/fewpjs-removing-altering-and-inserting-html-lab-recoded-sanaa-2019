32b3b34f49f65b00068da4007e5bef605e2b3e1a
Mustafa Al shaheri <mustafa.alshaheri@gmail.com>
2019-10-22 09:40:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // Write your code here!
+// Write your code here!
 let removeMain = document.getElementById('main');
 
 removeMain.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id='victory';
+
+ document.getElementById('victory').innerHTML="Mustafa Al Shaheri";
