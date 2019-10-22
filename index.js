49711ba49b79623bba0fd2972d987f93f8893204
Mustafa Al shaheri <mustafa.alshaheri@gmail.com>
2019-10-22 10:00:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let removeMain = document.getElementById('main');
+
+removeMain.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id='victory';
+
+newHeader.innerHTML="Mustafa Al Shaheri is the champion ";
